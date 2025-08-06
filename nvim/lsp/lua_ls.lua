197a2278793ece37e1ef85d3e2ecf9d92f20dbe7
MrRichar02 <richarmamadi03@gmail.com>
2025-08-06 11:21:20 -0500

    fix: adding config so that lua_ls understands vim global

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -70,6 +70,13 @@
 return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			}
+		}
+	},
   root_markers = {
     '.luarc.json',
     '.luarc.jsonc',
