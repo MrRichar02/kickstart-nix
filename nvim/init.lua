b77f562b9bfa54bd0c63056ff09b9aa26d610909
MrRichar02 <richarmamadi03@gmail.com>
2025-08-06 13:37:41 -0500

    fixing order to require files in init.lua and adding a keybind for lsp formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
-require("config.lsp")
-require("config.keyMaps")
 require("config.options")
+require("config.keyMaps")
+require("config.lsp")
--- nvim/lua/config/keyMaps.lua
+++ nvim/lua/config/keyMaps.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '<C-n>', '<cmd>:Ex<CR>')
+vim.keymap.set('n', '<leader>ft', vim.lsp.buf.format )
