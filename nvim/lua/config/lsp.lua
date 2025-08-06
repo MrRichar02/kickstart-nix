c0c7f65f911e265edd0faa8c7e02e23ba46ac31b
MrRichar02 <richarmamadi03@gmail.com>
2025-08-06 11:23:26 -0500

    adding blink.cmp and commenting some now unnecessary config for completion

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -4,20 +4,22 @@ vim.lsp.enable({
 	'nixd',
 	'jdtls',
 	'basedpyright',
+	'superhtml',
+	-- 'html',
+	-- 'htmlls',
 })
 
 -- diagnostics config for lsps
 vim.diagnostic.config({ virtual_text = { current_line = true } })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-			vim.keymap.set('i', '<C-Space>', vim.lsp.completion.get)
-    end
-  end,
-})
-vim.cmd("set completeopt+=noselect")
-
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client:supports_method('textDocument/completion') then
+--       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+-- 			vim.keymap.set('i', '<C-Space>', vim.lsp.completion.get)
+--     end
+--   end,
+-- })
+-- vim.cmd("set completeopt+=noselect")
